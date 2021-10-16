2934acdf170de7e108e9c46974c84ea699f3163c
Caio Sain Vallio <caiosainvallio@me.com>
2021-10-15 21:30:04 -0300

    Add files via upload

---
+++ strings.jl
@@ -0,0 +1,36 @@
+# Literals ----------------------------------------------------
+# empacotamento
+x, y, z = "um", "dois", "tres"
+
+a, b = 1, 2
+a, b = b, a 
+
+# concatenate *
+x*y*z 
+
+# repeat ^
+x^4
+
+# LaTeX
+α = 2
+β = 5
+ϵ = 2
+X = 45
+
+α + β*X + ϵ
+
+# unicode
+'a'
+'a' + 1
+
+# Scape Sequences --------------------------------------------------
+"\$"
+
+x = "He said \"Hello, world\""; println(x)
+x = "Hello, \nworld"; println(x)
+
+doggerel = "Errors are red, \nSome things are blue, \nI love coding \nAnd so should you"; println(doggerel)
+
+
+
+
