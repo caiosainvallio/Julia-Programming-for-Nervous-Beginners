2690e12ecd2f62bbe8302878732cad01165c6f56
Caio Sain Vallio <caiosainvallio@me.com>
2021-10-16 15:43:18 -0300

    Add files via upload

---
+++ logical_operators.jl
@@ -0,0 +1,28 @@
+# Comparison and Logical Operators ------------------------------------------------------------
+
+a, b, c = 1, 2, -3
+a > b
+a > c
+a + b > c
+a + b >= -c
+
+!true
+!false
+
+true && true
+true && false
+false && true
+false && false
+
+true || true
+true || false
+false || true
+false || false
+
+
+!true && true
+!true || true
+
+!(true && true)
+!(true || true)
+
